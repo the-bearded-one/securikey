3cbe576c3e64bcce00df2d72d3bf2a0cc9ed31f3
Jason Young <jason.young@inarimedical.com>
2023-09-21 17:40:18 -0700

    WIP. Added Business Logic Project.

---
+++ app/BusinessLogic/Issue.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessLogic
+{
+    public class Issue
+    {
+        long Uid { get; set; }
+        Severity Severity { get; set; }
+        ExploitMaturity ExploitMaturity { get; set; }
+        string FixedIn { get; set; }
+        string RemediationRecommendation { get; set; }
+    }
+}
