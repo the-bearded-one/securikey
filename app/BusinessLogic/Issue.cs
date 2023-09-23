6a28f879f353f5ec9510defa4fc733fa2a68f22e
Jason Young <jason.young@inarimedical.com>
2023-09-23 06:06:39 -0700

    fixed rebase errors

--- app/BusinessLogic/Issue.cs
+++ app/BusinessLogic/Issue.cs
@@ -1,11 +1,33 @@
-﻿namespace BusinessLogic
+﻿using System.Text;
+
+namespace BusinessLogic
 {
     public class Issue
     {
-        long Uid { get; set; }
-        Severity Severity { get; set; }
-        ExploitMaturity ExploitMaturity { get; set; }
-        string FixedIn { get; set; }
-        string RemediationRecommendation { get; set; }
+        public Issue()
+        {
+            UID = Guid.NewGuid().ToString();
+        }
+
+        public string UID { get; set; }
+        public string Asset { get; set; }
+        public string Description { get; set; }
+        public Severity Severity { get; set; }
+        public ExploitMaturity ExploitMaturity { get; set; }
+        public string FixedIn { get; set; }
+        public string RemediationRecommendation { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"UID: {UID}");
+            sb.AppendLine($"Asset: {Asset}");
+            sb.AppendLine($"Description: {Description}");
+            sb.AppendLine($"Severity: {Severity}");
+            sb.AppendLine($"ExploitMaturity: {ExploitMaturity}");
+            sb.AppendLine($"FixedIn: {FixedIn}");
+            sb.AppendLine($"RemediationRecommendation: {RemediationRecommendation}");
+            return sb.ToString();
+        }
     }
 }
