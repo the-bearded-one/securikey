9b856c44f0750dfe56bbf4af564793f9256abb15
Jason Young <jason.young@inarimedical.com>
2023-09-22 20:08:49 -0700

    WIP added cvescanner

--- app/BusinessLogic/Issue.cs
+++ app/BusinessLogic/Issue.cs
@@ -2,10 +2,29 @@
 {
     public class Issue
     {
-        long Uid { get; set; }
-        Severity Severity { get; set; }
-        ExploitMaturity ExploitMaturity { get; set; }
-        string FixedIn { get; set; }
-        string RemediationRecommendation { get; set; }
+        public Issue()
+        {
+            // assign a guid on instantiation so we can uniquely identify the issue
+            GUID = System.Guid.NewGuid().ToString();
+        }
+
+        public string GUID { get; private set; } = string.Empty;
+        public string Asset { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Severity Severity { get; set; }
+        public ExploitMaturity ExploitMaturity { get; set; }
+        public string FixedIn { get; set; } = string.Empty;
+        public string RemediationRecommendation { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            string issueData = $"GUID: {GUID}\r\nAsset: {Asset}\r\n"+
+                $"Description: {Description}\r\n" +
+                $"Severity: {Severity.ToString()}\r\n" +
+                $"ExploitMaturity: {ExploitMaturity}\r\n" +
+                $"FixedIn: {FixedIn}" +
+                $"RemediationRecommendation: {RemediationRecommendation}";
+            return issueData;
+        }
     }
 }
---
+++ app/BusinessLogic/Scanning/ApplicationInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Scanning
+{
+    public class ApplicationInfo
+    {
+        public string DisplayName { get; set; }
+        public string DisplayVersion { get; set; }
+        public string Publisher { get; set; }
+    }
+}
