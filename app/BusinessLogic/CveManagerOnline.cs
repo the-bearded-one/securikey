fa2ffdfe00677385b301e2879c3e928d154b44bd
jyoung <22526524+dnide00@users.noreply.github.com>
2023-10-06 22:39:54 -0700

    added first cut at online cve manager. Also added unit test for PoC

---
+++ app/BusinessLogic/CveManagerOnline.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace BusinessLogic
+{
+    public class CveManagerOnline
+    {        
+        //NIST API URL that only outputs CVEs that are in CISA's Known Exploited Vulnerabilities catalog
+        string url = @"https://services.nvd.nist.gov/rest/json/cves/2.0";
+        public CveRoot CveData { get; set; }
+
+        public void Search(string keyword, string version)
+        {
+            try
+            {
+                var keywordParam = $"keywordSearch={keyword}";
+                var fullUrl = $"{url}?{keywordParam}&keywordExactMatch&hasKev";
+
+                var httpClient = new HttpClient();
+                var response = httpClient.GetAsync(fullUrl);
+
+                if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var cveData = response.Result.Content.ReadAsStringAsync().Result;
+
+                    CveData = JsonConvert.DeserializeObject<CveRoot>(cveData);
+                }
+                else
+                {
+                    Console.WriteLine($"Request failed with status code {response.Result.StatusCode}");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request failed: {e.Message}");
+            }
+        }
+    }
+}
