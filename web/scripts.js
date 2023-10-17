536af366b6f4c329f1dc1d500fdf7255de93eabf
Mark Mengelt <mmengelt@berkeley.edu>
2023-10-17 09:58:41 -0400

    fixed carosel

--- web/scripts.js
+++
@@ -1,8 +0,0 @@
-
-// Use this file to add JavaScript to your project
-
-<script>
-  // Initialize the carousel
-  $(document).ready(function () {
-    $('#carouselDarkVariant').carousel()});
-</script>
